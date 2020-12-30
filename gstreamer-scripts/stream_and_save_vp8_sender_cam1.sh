2e5c0f341c2f0d655ecad6b87ffa257b72558c75
Alex Rau <rau.alex@gmail.com>
2020-12-30 11:53:53 +0100

    Added scripts for gstreamer over network

---
+++ gstreamer-scripts/stream_and_save_vp8_sender_cam1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gst-launch-1.0 v4l2src extra-controls=s,exposure_auto=3,exposure_absolute=30,brightness=100,backlight_compensation=1 device=/dev/video1 ! video/x-raw,width=640,height=480,framerate=15/2,format=YUY2 ! videorate ! video/x-raw,framerate=4/1 ! nvvidconv ! 'video/x-raw(memory:NVMM),format=NV12' ! nvv4l2vp8enc  ! matroskamux streamable=true ! tee name=t ! queue ! tcpclientsink host=127.0.0.1 port=10001 t. ! filesink location=stream1.webm
---
+++ gstreamer-scripts/stream_vp8_receiver_cam1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gst-launch-1.0 -t -v tcpserversrc host=0.0.0.0 port=10001 ! queue ! matroskademux ! vp8dec ! videoconvert ! ximagesink
+#gst-launch-1.0 -t -v tcpserversrc host=0.0.0.0 port=10001 ! queue ! matroskademux ! vp8dec ! videoconvert ! appsink
